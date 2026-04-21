c98ef26507f739626f17f2a56a065dfa59724b87
George-Njuguna <mailtomegeorge0@gmail.com>
2026-04-21 21:13:11 +0300

    .

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-from pydantic_settings import BaseSettings
+from pydantic import BaseSettings, PostgresDsn
 from dotenv import load_dotenv
 from pathlib import Path
 
@@ -8,21 +8,7 @@ load_dotenv( env_path )
 
 class Settings(BaseSettings):
 
-    DB_USER: str
-    DB_PW: str
-    DB_HOST: str
-    DB_PORT: int
-    DB_NAME: str
-
-    def DB_URL(self) -> str:
-        return (
-            f"postgresql://{self.DB_USER}:"
-            f"{self.DB_PW}@"
-            f"{self.DB_HOST}:"
-            f"{self.DB_PORT}/"
-            f"{self.DB_NAME}"
-        )
-
+    DB_URL = PostgresDsn
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "DataIngestion"
 

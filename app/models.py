c74448806dd2fb9903ff4470066f632160946318
George-Njuguna <mailtomegeorge0@gmail.com>
2026-04-22 14:58:27 +0300

    .

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Float,DateTime
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users" 
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String)
+    user_email = Column(String, unique=True)
+    password = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
+
+class Product(Base):
+    __tablename__ = "products" 
+
+    product_id = Column(Integer, primary_key=True, index=True)
+    product_name = Column(String)
+    product_details = Column(String)
+    price = Column(Float)

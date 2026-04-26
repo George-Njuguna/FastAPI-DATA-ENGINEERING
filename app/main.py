13b62f31f2cc3af0348206a6c8d15c85cb16c5b9
George-Njuguna <mailtomegeorge0@gmail.com>
2026-04-26 12:50:00 +0300

    .

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from pydantic.functional_validators import AfterValidator
 from uuid import uuid4, UUID
 from sqlalchemy.orm import Session
 from datetime import datetime
-from . import schemas, db, crud 
+from . import schemas, db, crud, services
 
 # Simulating Database
 Users_db = []
@@ -38,6 +38,11 @@ def PostProduct(product_info : schemas.ProductUpdate, storage = Depends(db.get_d
 def GetProductInfo( product_id : int, storage = Depends(db.get_db)):
     logger.info(f"Getting info of product {product_id}")
     return( crud.getProductbyId( db = storage , product_id = product_id ) )
+
+@app.get("/total-products/")
+def GetNumberofProducts( storage = Depends(db.get_db)):
+    logger.info(f"Getting Total products")
+    return( services.get_total_products( db = storage ) )
     
 
 #---------------------------
@@ -55,3 +60,10 @@ def CreateNewUserAccount(user_info : schemas.UserCreate, storage = Depends(db.ge
 def GetUserInfo( user_id : int, storage = Depends(db.get_db)):
     logger.info(f"Getting info of User {user_id}")
     return( crud.getUserbyId( db = storage , user_id = user_id ) )
+
+@app.get("/total-user/")
+def GetNumberofUsers( storage = Depends(db.get_db)):
+    logger.info(f"Getting Total Users")
+    return( services.get_total_users( db = storage ) )
+
+
---
+++ app/services.py
@@ -0,0 +1,21 @@
+from . import models , schemas
+from sqlalchemy.orm import Session
+from sqlalchemy import select,func
+
+def get_total_users( db : Session ):
+
+    stmt = select(func.count(models.User.user_id))
+
+    result = db.scalar(stmt)
+
+
+    return result
+
+
+def get_total_products( db : Session ):
+
+    stmt = select(func.count(models.Product.product_id))
+
+    result = db.scalar(stmt)
+
+    return result

fc9695056b749ff8b50758ef3236482691a8434d
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-01 11:15:20 +0300

    changed the names of columns

--- app/models.py
+++ app/models.py
@@ -8,17 +8,17 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = "users" 
 
-    user_id = Column(Integer, primary_key=True, index=True)
-    user_name = Column(String)
-    user_email = Column(String, unique=True)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String, unique=True)
     password = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
 class Product(Base):
     __tablename__ = "products" 
 
-    product_id = Column(Integer, primary_key=True, index=True)
-    product_name = Column(String)
-    product_details = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    details = Column(String)
     price = Column(Float)
     created_at = Column(DateTime(timezone=True), server_default=func.now())

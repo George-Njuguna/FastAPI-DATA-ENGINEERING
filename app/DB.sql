faf70a0df4d111499ca1b6d8c65f73f3c96ce420
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-01 11:23:42 +0300

    .

--- app/DB.sql
+++ app/DB.sql
@@ -1,17 +1,17 @@
 CREATE TABLE IF NOT EXISTS users(
-    user_id SERIAL PRIMARY KEY,
-    user_name VARCHAR(100) NOT NULL,
-    user_email VARCHAR(100) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
     password VARCHAR(100) NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    UNIQUE(user_email)
+    UNIQUE(email)
 );
 
 CREATE TABLE IF NOT EXISTS products(
-    product_id SERIAL PRIMARY KEY,
-    product_name VARCHAR(100) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     product_details VARCHAR(200) NOT NULL,
-    price FLOAT, 
+    price INT, 
     created_at TIMESTAMPTZ DEFAULT NOW()
 );
 

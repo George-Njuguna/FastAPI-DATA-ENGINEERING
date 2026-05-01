6d9234bc21ca86f46ea8b2d6afecb52d5d5f8b28
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-01 11:34:07 +0300

    .

--- app/crud.py
+++ app/crud.py
@@ -2,55 +2,50 @@ from . import models , schemas
 from sqlalchemy.orm import Session
 from sqlalchemy import select,func 
 
-def create_user(db: Session, user: schemas.UserCreate):
+def add_user(db: Session, user: schemas.UserCreate):
     # 1. Map Schema -> Model
     db_user = models.User(
-        user_name = user.user_name,
-        user_email = user.user_email,
+        user_name = user.name,
+        user_email = user.email,
         password = user.password
     )
     # 2. Stage and Commit
     db.add(db_user)
-    db.flush()
 
     return db_user
 
-def getUserbyId(db : Session, user_id : int):
+def getUserbyId(db : Session, id : int):
 
-    stmt = select( models.User ).where(models.User.user_id == user_id )
-
+    stmt = select( models.User ).where(models.User.user_id == id )
     result = db.execute(stmt)
-
 
     return result.scalar_one_or_none()
 
-def getProductbyId(db : Session, product_id : int):
-    stmt = select( models.Product ).where(models.Product.product_id == product_id)
+def getProductbyId(db : Session, id : int):
 
+    stmt = select( models.Product ).where(models.Product.id == id)
     result = db.execute(stmt)
 
     return result.scalar_one_or_none() 
 
 
-def create_product( db : Session, product : schemas.ProductBase):
+def add_product( db : Session, product : schemas.ProductBase):
 
     db_product = models.Product(
-        product_name = product.product_name,
+        product_name = product.name,
         price = product.price,
         product_details = product.product_details
     )
 
     db.add(db_product)
-    db.flush()
+
     return db_product
 
 
 def get_total_users( db : Session ):
 
-    stmt = select(func.count(models.User.user_id))
-
+    stmt = select(func.count(models.User.id))
     result = db.scalar(stmt)
-
 
     return result
 
@@ -58,8 +53,7 @@ def get_total_users( db : Session ):
 
 def get_total_products( db : Session ):
 
-    stmt = select(func.count(models.Product.product_id))
-
+    stmt = select(func.count(models.Product.id))
     result = db.scalar(stmt)
 
     return result

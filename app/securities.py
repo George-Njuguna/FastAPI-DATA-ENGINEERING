7a2d652a74c09babab6201c247b698d29b60abd2
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-18 08:43:52 +0300

    added create access token

--- app/securities.py
+++ app/securities.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from datetime import datetime, timezone, timedelta
+from jose import jwt
+from .config import settings
 
 pwd_context = CryptContext(
     schemes=["bcrypt"], # this is the hashing 
@@ -14,3 +17,21 @@ class SecurityHandler:
     @staticmethod
     def  verify_password( str_password : str , hashed_password : str ) -> bool:
         return pwd_context.verify( str_password , hashed_password )
+    
+
+def create_access_token( data : dict, expiry_delta : timedelta | None = None ) -> str:
+
+    if expiry_delta:
+        expire = datetime.now ( timezone.utc ) + expiry_delta
+
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    data.update({"exp": int(expire.timestamp())})
+
+    encoded_jwt = jwt.encode(
+        data, 
+        settings.SECRET_KEY, 
+        algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt

921698da92eb4bde453604f2ac20848ebaef949a
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-18 18:16:41 +0300

    .

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import PostgresDsn
+from pydantic import Field
 from pathlib import Path
 
 
@@ -14,14 +15,14 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     PROJECT_NAME: str = "DataIngestion"
 
-    SECRET_KEY : str = os.get_env("JWT_SECRET_KEY", "DEVELOPMENT_INSECURE_FALLBACK_KEY_MUST_CHANGE")
+    SECRET_KEY: str = Field(validation_alias="JWT_SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30    
 
     model_config = SettingsConfigDict(
         env_file = str(ENV_FILE),
         env_file_encoding = 'utf-8',
-        case_sensitiv = True  
+        case_sensitive = True  
     )
 
 

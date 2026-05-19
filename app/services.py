45a98095df92a78b2b0b78c6e40440789922fb64
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-19 07:27:35 +0300

    .

--- app/services.py
+++ app/services.py
@@ -1,7 +1,39 @@
-from . import crud, schemas
+from . import crud, schemas, auth, securities 
 from sqlalchemy.orm import Session
 from datetime import datetime
-from fastapi import HTTPException
+from fastapi import HTTPException , status
+
+
+# ----------------------
+# AUTHENTICATION
+# ----------------------
+class AuthService:
+    @staticmethod
+
+    def auth_user_and_create_token(storage: Session, data: schemas.LoginRequest) -> schemas.TokenResponse:
+
+        user = crud.get_user_by_email(storage, data.email)
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail="Invalid Email "
+            )
+        
+        # check password 
+        valid_password = securities.SecurityHandler.verify_password( data.password, user.password )
+        if not valid_password:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, 
+                detail="Invalid Password"
+            )
+        
+        # creating acess token 
+        access_token = securities.SecurityHandler.create_access_token( data = {"sub": user.email} )
+
+        return schemas.TokenResponse(
+            access_token=access_token, 
+            token_type="bearer"
+        )
 
 # -----------------------
 # USER

2d2172f46366fc551acb220e0566abca84ab4e9d
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-26 15:28:11 +0300

    .

--- app/main.py
+++ app/main.py
@@ -6,12 +6,17 @@ from pydantic.functional_validators import AfterValidator
 from uuid import uuid4, UUID
 from sqlalchemy.orm import Session
 from datetime import datetime
-from . import schemas, db, services
+from . import schemas, db, services, auth
 
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+allow_engineers = auth.RoleChecker(["ADMIN","ENGINEER"])
+allow_analysts = auth.RoleChecker(["ADMIN","ANALYST"])
+allow_viewers = auth.RoleChecker(["ADMIN","ENGINEER","ANALYST","VIEWER"])
+
 
 
 @app.get("/")
@@ -32,23 +37,23 @@ def login( login_info : schemas.LoginRequest , storage = Depends(db.get_db)):
 # PRODUCT ENDPOINTS
 #------------------------
 
-@app.post("/product/", response_model = schemas.ProductOut)
-def PostProduct(product_info : schemas.ProductCreate, storage = Depends(db.get_db)):
+@app.post("/product/", response_model = schemas.ProductOut )
+def PostProduct(product_info : schemas.ProductCreate, storage = Depends(db.get_db), token_context: dict = Depends(allow_engineers)):
     logger.info(f"New product Added")
     return services.ProductService.create_product( db = storage, data = product_info)
 
 @app.get("/products/{id}", response_model = schemas.ProductOut)
-def GetProductInfo( id : int, storage = Depends(db.get_db)):
+def GetProductInfo( id : int, storage = Depends(db.get_db), token_context: dict = Depends(allow_viewers)):
     logger.info(f"Getting info of product {id}")
     return( services.ProductService.get_product_by_id( db = storage , id = id ) )
 
 @app.get("/total-products/", response_model = schemas.ProductStats)
-def GetNumberofProducts( storage = Depends(db.get_db)):
+def GetNumberofProducts( storage = Depends(db.get_db), token_context: dict = Depends(allow_viewers)):
     logger.info(f"Getting Total products")
     return( services.ProductService.get_total_products( db = storage ) )
 
 @app.post("/products/", response_model = schemas.BulkProductLoad )
-def BulkProductsLoad( products  : List[schemas.ProductCreate], storage = Depends(db.get_db)):
+def BulkProductsLoad( products  : List[schemas.ProductCreate], storage = Depends(db.get_db), token_context: dict = Depends(allow_engineers)):
     logger.info(f"Loading Bulk Data")
     return( services.ProductService.create_bulk_products(db = storage, data = products))
     
@@ -65,12 +70,12 @@ def CreateNewUserAccount(user_info : schemas.UserCreate, storage = Depends(db.ge
 
 
 @app.get("/user/{id}", response_model = schemas.UserOut)
-def GetUserInfo( id : int, storage = Depends(db.get_db)):
+def GetUserInfo( id : int, storage = Depends(db.get_db), token_context: dict = Depends(allow_analysts)):
     logger.info(f"Getting info of User {id}")
     return( services.UserService.get_user_by_id( db = storage , id = id ) )
 
 @app.get("/total-user/", response_model = schemas.UserStats)
-def GetNumberofUsers( storage = Depends(db.get_db)):
+def GetNumberofUsers( storage = Depends(db.get_db), token_context: dict = Depends(allow_analysts)):
     logger.info(f"Getting Total Users")
     return( services.UserService.get_total_users( db = storage ) )
 

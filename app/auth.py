05be9b7e4e3fa21eaaec162d7b057b630ecf1086
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-26 14:57:29 +0300

    added RoleChecker

--- app/auth.py
+++ app/auth.py
@@ -24,3 +24,26 @@ def get_user_email(token : str = Depends(oauth2_scheme)) -> str :
     except JWTError:
         raise credentials_exception
     
+class RoleChecker:
+
+    def __init__(self , allowed_roles : list[str]):
+        self.allowed_roles = allowed_roles
+
+    def __call__(self, token : str = Depends(oauth2_scheme)) -> dict:
+
+        exception = HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Operation rejected: Insufficient pipeline permissions."
+        )
+        
+        try:
+
+            payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+            user_role : str  = payload.get("role")
+
+            if user_role is not self.allowed_roles:
+                raise exception 
+            
+        except JWTError:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token context")
+        

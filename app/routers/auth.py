00094de9401dade7dfa58aedd2d60df8cb3e14d5
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-27 20:43:11 +0300

    .

---
+++ app/routers/auth.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Depends, status
+from .. import schemas, db 
+from ..services import auth
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+router = APIRouter(
+    prefix="/login",
+    tags=["login Authentification"] # Groups endpoints together neatly in Swagger UI
+)
+
+@router.post("/login/", response_model = schemas.TokenResponse)
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(db.get_db)):
+
+    credentials = schemas.LoginRequest(
+        email=form_data.username,
+        password=form_data.password
+    )
+
+    return auth.AuthService.auth_user_and_create_token(db, credentials)

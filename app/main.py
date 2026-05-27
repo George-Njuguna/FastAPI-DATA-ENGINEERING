13aafcd5c4a2955a44d5731774f15af5d721e1f6
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-27 21:18:23 +0300

    .

--- app/main.py
+++ app/main.py
@@ -29,41 +29,11 @@ def health_check():
     return {"status": "healthy", "environment": "production"}
 
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-allow_engineers = auth.RoleChecker(["admin","engineer"])
-allow_analysts = auth.RoleChecker(["admin","analyst"])
-allow_viewers = auth.RoleChecker(["admin","engineer","analyst","viewer"])
-
-
-
 @app.get("/")
 def root():
     return {
         "message": "e-commerse  API is running" 
     }
 
-
-#---------------------------
-# USER ENDPOINTS
-#---------------------------
-
-@app.post("/create-user/", response_model = schemas.UserOut)
-def CreateNewUserAccount(user_info : schemas.UserCreate, storage = Depends(db.get_db)):
-
-    logger.info(f"New Account Created")
-    return services.UserService.create_user( db = storage, data = user_info)
-
-
-@app.get("/user/{id}", response_model = schemas.UserOut)
-def GetUserInfo( id : int, storage = Depends(db.get_db), token_context: dict = Depends(allow_analysts)):
-    logger.info(f"Getting info of User {id}")
-    return( services.UserService.get_user_by_id( db = storage , id = id ) )
-
-@app.get("/total-user/", response_model = schemas.UserStats)
-def GetNumberofUsers( storage = Depends(db.get_db), token_context: dict = Depends(allow_analysts)):
-    logger.info(f"Getting Total Users")
-    return( services.UserService.get_total_users( db = storage ) )
 
 
---
+++ app/routers/users.py
@@ -0,0 +1,35 @@
+from ..services import users, auth
+from .. import schemas, db 
+from fastapi import FastAPI ,HTTPException, Depends, APIRouter
+from typing import List
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+router = APIRouter(
+    prefix="/users",
+    tags=["Users Operations "] # Groups endpoints together neatly in Swagger UI
+)
+
+allow_engineers = auth.RoleChecker(["engineer","admin"])
+allow_viewers = auth.RoleChecker(["viewer","engineer","admin"])
+allow_admin = auth.RoleChecker(['admin'])
+allow_analysts = auth.RoleChecker(['admin','analyst'])
+
+@router.post("/create-user/", response_model = schemas.UserOut)
+def CreateNewUserAccount(user_info : schemas.UserCreate, storage = Depends(db.get_db)):
+
+    logger.info(f"New Account Created")
+    return users.UserService.create_user( db = storage, data = user_info)
+
+
+@router.get("/user/{id}", response_model = schemas.UserOut)
+def GetUserInfo( id : int, storage = Depends(db.get_db), token_context: dict = Depends(allow_analysts)):
+    logger.info(f"Getting info of User {id}")
+    return( users.UserService.get_user_by_id( db = storage , id = id ) )
+
+@router.get("/total-user/", response_model = schemas.UserStats)
+def GetNumberofUsers( storage = Depends(db.get_db), token_context: dict = Depends(allow_analysts)):
+    logger.info(f"Getting Total Users")
+    return( users.UserService.get_total_users( db = storage ) )

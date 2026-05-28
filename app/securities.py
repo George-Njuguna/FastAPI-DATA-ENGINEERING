c17bee0b4e2f1e8a8b723c922719fe2224241410
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-28 22:04:29 +0300

    .

--- app/securities.py
+++ app/securities.py
@@ -1,4 +1,5 @@
 import bcrypt
+from fastapi import HTTPException, status
 from datetime import datetime, timezone, timedelta
 from jose import jwt
 from .config import settings
@@ -22,16 +23,19 @@ class SecurityHandler:
             hashed_password.encode("utf-8")
     )
     
-
+    @staticmethod
     def create_access_token( data : dict, expiry_delta : timedelta | None = None ) -> str:
 
         if expiry_delta:
             expire = datetime.now ( timezone.utc ) + expiry_delta
 
         else:
-            expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+            expire = datetime.now( timezone.utc ) + timedelta( minutes = settings.ACCESS_TOKEN_EXPIRE_MINUTES )
 
-        data.update({"exp": int(expire.timestamp())})
+        data.update(
+            {"exp": int( expire.timestamp()),
+             "type": "access"}
+            )
 
         encoded_jwt = jwt.encode(
             data, 
@@ -39,4 +43,27 @@ class SecurityHandler:
             algorithm=settings.ALGORITHM
         )
         return encoded_jwt
+    
+    @staticmethod
+    def create_refresh_token( data : dict ) -> str:
+
+        expire = datetime.now( timezone.utc ) + timedelta( days = settings.ACCESS_TOKEN_EXPIRE_MINUTES )
+        data.update(
+            {"exp": int( expire.timestamp() ),
+             "type": "refresh"}
+            )
+        
+        encoded_jwt = jwt.encode(
+            data, 
+            settings.SECRET_KEY, 
+            algorithm=settings.ALGORITHM
+        )
+        return encoded_jwt
+    
+
+
+
+
+
+
 

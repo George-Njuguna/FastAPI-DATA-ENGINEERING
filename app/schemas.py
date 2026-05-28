4377f2dd93fa59944b7eace295b777ca27a1dc95
George-Njuguna <mailtomegeorge0@gmail.com>
2026-05-28 21:40:10 +0300

    added refresh token to TokenResponse

--- app/schemas.py
+++ app/schemas.py
@@ -24,6 +24,7 @@ class LoginRequest(BaseModel):
 # -----------------------
 class TokenResponse(BaseModel):
     access_token : str
+    refresh_token : str
     token_type : str 
 
 #------------------------------

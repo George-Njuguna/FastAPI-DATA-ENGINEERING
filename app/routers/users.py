256156373a74babe29eff645e447571ee0e95c65
George-Njuguna <mailtomegeorge0@gmail.com>
2026-06-03 11:39:57 +0300

    Working User routes

--- app/routers/users.py
+++ app/routers/users.py
@@ -12,19 +12,25 @@ router = APIRouter(
     tags=["Users Operations "] # Groups endpoints together neatly in Swagger UI
 )
 
+
 @router.post("/create-user/", response_model = schemas.UserOut)
 def CreateNewUserAccount(user_info : schemas.UserCreate, storage = Depends(db.get_db) , current_user = Depends(auth.RoleChecker(["engineer","admin"]))):
 
     logger.info(f"New Account Created")
-    return users.UserService.create_user( db = storage, data = user_info)
+    service = users.UserService( db = storage )
+    return service.create_user(data = user_info)
 
 
 @router.get("/user/{id}", response_model = schemas.UserOut)
 def GetUserInfo( id : int, storage = Depends(db.get_db), current_user = Depends(auth.RoleChecker(["engineer","admin"]))):
+
     logger.info(f"Getting info of User {id}")
-    return( users.UserService.get_user_by_id( db = storage , id = id ) )
+    service = users.UserService( db = storage )
+    return( service.get_user_by_id( id = id ) )
 
 @router.get("/total-user/", response_model = schemas.UserStats)
 def GetNumberofUsers( storage = Depends(db.get_db), current_user = Depends(auth.RoleChecker(["analyst","admin"]) ) ):
+
     logger.info(f"Getting Total Users")
-    return( users.UserService(storage).get_total_users())
+    service = users.UserService( db = storage )
+    return( service.get_total_users() )

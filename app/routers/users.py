082ba2fea92b28cb35807365308df75ca8459cf3
George-Njuguna <mailtomegeorge0@gmail.com>
2026-06-03 12:44:53 +0300

    added dependancies.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,7 @@
-from ..services import users, auth
+from ..services.users import UserService
+from ..services.auth import RoleChecker
 from .. import schemas, db 
+from ..dependancies.users import get_user_service
 from fastapi import FastAPI ,HTTPException, Depends, APIRouter
 from typing import List
 import logging
@@ -14,23 +16,31 @@ router = APIRouter(
 
 
 @router.post("/create-user/", response_model = schemas.UserOut)
-def CreateNewUserAccount(user_info : schemas.UserCreate, storage = Depends(db.get_db) , current_user = Depends(auth.RoleChecker(["engineer","admin"]))):
+def CreateNewUserAccount(
+    user_info : schemas.UserCreate, 
+    service : UserService = Depends( get_user_service ) , 
+    current_user = Depends( RoleChecker(["engineer","admin"]) )
+    ):
 
     logger.info(f"New Account Created")
-    service = users.UserService( db = storage )
+
     return service.create_user(data = user_info)
 
 
 @router.get("/user/{id}", response_model = schemas.UserOut)
-def GetUserInfo( id : int, storage = Depends(db.get_db), current_user = Depends(auth.RoleChecker(["engineer","admin"]))):
+def GetUserInfo( 
+    id : int,
+    service : UserService = Depends( get_user_service ) , 
+    current_user = Depends(RoleChecker(["engineer","admin"]))
+    ):
 
     logger.info(f"Getting info of User {id}")
-    service = users.UserService( db = storage )
     return( service.get_user_by_id( id = id ) )
 
 @router.get("/total-user/", response_model = schemas.UserStats)
-def GetNumberofUsers( storage = Depends(db.get_db), current_user = Depends(auth.RoleChecker(["analyst","admin"]) ) ):
+def GetNumberofUsers( 
+    service : UserService = Depends( get_user_service ) , 
+    current_user = Depends(RoleChecker(["analyst","admin"]) ) ):
 
     logger.info(f"Getting Total Users")
-    service = users.UserService( db = storage )
     return( service.get_total_users() )

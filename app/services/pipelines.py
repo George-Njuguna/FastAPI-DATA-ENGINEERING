2c761cebedaf19d4788bd3856f6684134e574985
George-Njuguna <mailtomegeorge0@gmail.com>
2026-06-11 16:27:06 +0300

    FileIngestionService

--- app/services/pipelines.py
+++ app/services/pipelines.py
@@ -1,17 +1,21 @@
 from ..db import SessionLocal
-from ..schemas import BulkResponse
-from .. import models
+from .. import models, schemas
 
 import io
 import pandas as pd
 from sqlalchemy import insert
 from sqlalchemy.orm import Session
+import logging
+import uuid
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class FileIngestionService:
    
     @staticmethod
-    def bulk_insert_csv_stream(cls, file_bytes: bytes, db: Session = None, chunk_size: int = 10000):
+    def bulk_insert_csv_stream(cls, file_bytes: bytes, triggered_by: str , db: Session = None, chunk_size: int = 10000):
 
         # Checking if Session db session has been loaded 
         is_independent_task = (db is None)
@@ -41,7 +45,12 @@ class FileIngestionService:
                 total_inserted += len(records)
                 
             db.commit()
-            return total_inserted
+            logger.info("INSERTING RECORDS")
+            return schemas.BulkResponse(
+                status = "Queued" if is_independent_task else "sucessfully loaded",
+                job_id = str(uuid.uuid4()),
+                triggered_by = triggered_by,
+            )
             
         except Exception as e:
             db.rollback()
@@ -50,6 +59,7 @@ class FileIngestionService:
         finally:
             # closing the connection 
             if is_independent_task:
+                logger.info("CLOSING THE CURRENT DATABASE CONNECTION")
                 db.close()
 
    

68e258b368c6b2464fc6b67531906aaf40ff1bdd
George-Njuguna <mailtomegeorge0@gmail.com>
2026-06-11 14:24:33 +0300

    added FileIngestionService in pipelines

--- app/services/pipelines.py
+++ app/services/pipelines.py
@@ -9,15 +9,20 @@ from sqlalchemy.orm import Session
 
 
 class FileIngestionService:
-
-    
+   
     @staticmethod
-    def bulk_insert_csv_stream(db: Session, file_bytes: bytes, chunk_size: int = 10000):
-        
-        # io.BytesIO turns raw bytes into a file-like object that Pandas can read
-        file_wrapper = io.BytesIO(file_bytes)
+    def bulk_insert_csv_stream(cls, file_bytes: bytes, db: Session = None, chunk_size: int = 10000):
+
+        # Checking if Session db session has been loaded 
+        is_independent_task = (db is None)
+
+        if is_independent_task:
+
+            db = SessionLocal() # this is for Asyncronised tasks
         
         try:
+            # io.BytesIO turns raw bytes into a file-like object that Pandas can read
+            file_wrapper = io.BytesIO(file_bytes)           
             # Read the CSV in manageable chunks (Memory-safe)
             # This ensures your server never uses more than a few MBs of RAM
             csv_reader = pd.read_csv(file_wrapper, chunksize=chunk_size)
@@ -36,30 +41,15 @@ class FileIngestionService:
                 total_inserted += len(records)
                 
             db.commit()
-            print(f"[FILE INGESTION SUCCESS] Safely uploaded {total_inserted} rows.")
             return total_inserted
             
         except Exception as e:
             db.rollback()
-            print(f"[FILE INGESTION CRASH] Data rolled back: {str(e)}")
             raise e
-
-    @staticmethod
-    def bulk_insert_json_stream(db: Session, file_bytes: bytes):
-        """
-        Reads raw JSON bytes, parses the array, and bulk-inserts the records.
-        """
-        file_wrapper = io.BytesIO(file_bytes)
-        try:
-            # Read JSON file straight into a dataframe
-            df = pd.read_json(file_wrapper)
-            records = df.to_dict(orient="records")
             
-            if records:
-                db.execute(insert(models.Product), records)
-                db.commit()
-                
-            return len(records)
-        except Exception as e:
-            db.rollback()
-            raise e
+        finally:
+            # closing the connection 
+            if is_independent_task:
+                db.close()
+
+   
